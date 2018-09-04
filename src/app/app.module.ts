0b88d0e158ded7880c6b1d74066fe6f5ee9d1448
Champion <escorpio17sc@gmail.com>
2018-09-04 13:53:49 -0500

    Se creó los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './componentes/home-page/home-page.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { RegisterPageComponent } from './componentes/register-page/register-page.component';
+import { LoginPageComponent } from './componentes/login-page/login-page.component';
+import { NoFoundPageComponent } from './componentes/no-found-page/no-found-page.component';
+import { PrivadoPageComponent } from './componentes/privado-page/privado-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavbarComponent,
+    RegisterPageComponent,
+    LoginPageComponent,
+    NoFoundPageComponent,
+    PrivadoPageComponent
   ],
   imports: [
     BrowserModule,
